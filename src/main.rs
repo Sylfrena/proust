ad16b57ed0797329f655da947f9badb96c4bbfb6
Sumera Priyadarsini <sylphrenadin@gmail.com>
2021-06-21 18:45:37 +0530

    Add cpuinfo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #[allow(dead_code)]
 mod meminfo;
+mod cpuinfo;
 
 mod proc_status;
 //mod meminfo_wh;
 
 fn main() {
     println!("Hello, sumoworld!");
+
+    //let a = cpuinfo::get_cpuinfo();
+
+    //println!("{:#?}", a[3].apicid); 
+
     //let a = meminfo::get_meminfo();
 
-    let b = proc_status::get_pid(90);
+    /*let b = proc_status::get_pid(68789);
     match b {
         Ok(t) => println!("{:#?}", t),
         Err(e) => println!("Error: {:#?}", e),
-    }
+    }*/
     //assert_eq!(meminfo_wh::get_meminfo(), meminfo::get_meminfo());
+
 }
